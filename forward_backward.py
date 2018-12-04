d211be25efe904702fef00956640729bca22d6f9
Yiwen Xu <yiwen@Yiwens-MBP.wv.cc.cmu.edu>
2018-12-03 21:00:07 -0500

    update EM.py

--- forward_backward.py
+++ forward_backward.py
@@ -1,16 +1,20 @@
-initialState=[1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9)]
-transition=[]
-emission=[]
 def initialization():
+    initialState=[1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9), 1/float(9)]
+    transition=[]
+    emission=[]
     for index in range(0,9):
         transition.append(initialState)
 
     for i in range(0,9):
         emission.append([1/float(4),1/float(4),1/float(4),1/float(4)])
+    return initialState, transition, emission
 
-def forward(s):
+def forward(s, initialState, transition, emission):
+    """
+    s: observation sequences
+    """
     dictionary = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-    result = [[0]*len(s) for i in range(9)]
+    result = [[0 for j in range(len(s))] for i in range(9)]
     for i in range(0, len(initialState)):
         result[i][0] = initialState[i]*emission[i][dictionary[s[0]]]
         
@@ -21,10 +25,11 @@ def forward(s):
                 total = total + result[temp][j-1] * transition[temp][z]
             result[z][j] = total * emission[z][dictionary[s[j]]]
     print("forward result "+str(result))
+    return result
 
-def backward(s):
+def backward(s, initialState, transition, emission):
     dictionary = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-    result = [[0]*len(s) for i in range(9)]
+    result = [[0 for j in range(len(s))] for i in range(9)]
     for index in range(0, len(initialState)):
         result[index][len(s)-1] = 1
 
@@ -37,6 +42,7 @@ def backward(s):
             result[z][j] = total
         j = j - 1
     print("backward result "+str(result))
+    return result
         
 if __name__ == "__main__":
     initialization()
